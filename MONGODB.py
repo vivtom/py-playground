c2ec1c47d2f5ee1082c90dc7626b27e25bc2079d
vivtom <vtomar155@gmail.com>
2025-01-25 08:30:09 +0530

    MONGODB python

--- MONGODB.py
+++ MONGODB.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 
-# Replace with your MongoDB connection string
 client = MongoClient('mongodb://localhost:27017/')
 
 db = client.todo_db  # Create or connect to a database named "todo_db"
