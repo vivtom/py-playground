e058e7d1eabd475f2eebde550ec44b230234799a
vivtom <vtomar155@gmail.com>
2025-01-26 18:37:00 +0530

    python practice

--- reverse_array.py
+++
@@ -1,8 +0,0 @@
-
-if __name__ == '__main__':
-    n = int(input().strip())
-    arr = list(map(int, input().rstrip().split()))
-    i = len(arr) - 1
-    while i >= 0:
-        print(arr[i], end= ' ')
-        i -= 1
