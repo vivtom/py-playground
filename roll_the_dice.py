b21fa442316c5c5a0827cfaec5e440c85aed1fe3
vivtom <vtomar155@gmail.com>
2025-05-17 17:06:05 +0530

    mini project

--- roll_the_dice.py
+++ roll_the_dice.py
@@ -3,7 +3,6 @@ def dice():
     while True:
         x = input('Roll the dice? y/n: ')
         if x == 'y':
-            num = 0
             a,b = (random.randint(1,6),random.randint(1,6))
 
             ''' Was doing this earlier, but it increases time complexity
