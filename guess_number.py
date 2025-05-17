8c2638ffd4a3e977da8f810cb13efe40073ea555
vivtom <vtomar155@gmail.com>
2025-05-17 17:53:10 +0530

    added features to the  Mini project

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,33 @@
+''' GAME - GUESS THE NUMBER. 
+
+Added multiple features :
+1- A feature of allowing the user to specify the minimum and maximum values for the number
+range before the game starts. This gives the player more control over the
+difficulty level.
+2- A feature that limits the number of guesses a player can make. If
+the player runs out of attempts, the game should end, and the correct number
+should be revealed. 
+3- A feature that keeps track of the fewest attempts it took to guess the
+number correctly. The program should display this "best score" at the end of
+each game. '''
+
 import random
 def guess():
-    x = random.randint(0,100)
-    count = 0
+    print('''NOTE : YOU HAVE LIMITED CHANCES.\nEnter the range you want to play. The higher the range, the more difficult it will be.''')
+    low = int(input('From? (enter a valid number): '))
+    high = int(input('to? (enter a valid number): '))
+    x = random.randint(low,high)
+    count = 1
+    best_score = float('inf')
     try:
         while True:
-            a = int(input('enter number: '))
-            if a < x:
+            a = int(input('Guess a number: '))
+            if count == 10:
+                print(f'You lose! The number was {x} ')
+                best_score = min(count,best_score)
+                print(f'Your best score is {best_score}')
+                break
+            elif a < x:
                 print('try again, number too low')
                 count += 1
             elif a > x:
@@ -13,7 +35,9 @@ def guess():
                 count += 1
             else:
                 print('CONGRATULATIONS, YOU ARE CORRECT')
-                print(f'You guess it in {count} chances')
+                print(f'You guessed it in {count} chances')
+                best_score = min(count,best_score)
+                print(f'Your best score is {best_score}')
                 break
     except Exception as e:
         print(f'enter a number! {e}')
